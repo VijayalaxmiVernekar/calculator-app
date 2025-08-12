5b0948711018d5aac6af4e6c8d7c68bc2fb13aaa
Vijayalaxmi.Vernekar <Vijayalaxmi.Vernekar@kletech.ac.in>
2025-08-12 13:31:54 +0530

    adding multiply operation

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     x, y = 10, 5
     print(f"Addition: {add(x, y)}")
     print(f"Subtraction: {subtract(x, y)}")
+    print(f"Multiplication: {multiply(x, y)}")
--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,11 @@
 import pytest
-from main import add, subtract
+from main import add, subtract , multiply
 
 def test_add():
     assert add(3, 2) == 5
 
 def test_subtract():
     assert subtract(5, 2) == 3
+
+def test_multiply():
+    assert multiply(5, 3) == 15
